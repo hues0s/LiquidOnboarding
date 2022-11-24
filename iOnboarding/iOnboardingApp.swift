ff91de70f63afe4539fab2df5295e33aa2a86900
Héctor Ullate <hector.ullate@gmail.com>
2022-11-24 11:07:16 +0100

    Initial Commit

---
+++ iOnboarding/iOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOnboardingApp.swift
+//  iOnboarding
+//
+//  Created by Héctor Ullate on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
